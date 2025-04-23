27c2f463e50c90de007bff2ab85331c6ee66096a
jose170696 <esteban_chz@hotmail.com>
2025-04-22 21:35:26 -0600

    Add project files.

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPP.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo no es válido.")]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "La clave es obligatoria.")]
+        public string Clave { get; set; }
+
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaAPP.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Tipo_usuario { get; set; }
+        public string Clave { get; set; }
+    }
+}
